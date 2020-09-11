527ee5cebfdde570e14cd85b1f06f00f7df48d5b
psaux <seriouz.accz@gmail.com>
2020-09-11 09:52:46 +0200

    Fix tests

--- src/fluent/FluentModelBuilder.spec.ts
+++ src/fluent/FluentModelBuilder.spec.ts
@@ -22,7 +22,7 @@ it('works', () => {
 
     const m = sut.of<Person>(PersonEntity)
 
-    const rootProps = [m.name, m.age, m.info, m.optionalInfo, m.optional]
+    const rootProps = [m.name, m.age, m.info, m.optionalInfo, m.optional, m.married]
     expect(m._meta.rootProps).toIncludeSameMembers(rootProps)
 
     expect(m._meta.props).toIncludeSameMembers([
