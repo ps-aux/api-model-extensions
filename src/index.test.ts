1ee035923815aae52c9457fd2a393a63307252ee
psaux <seriouz.accz@gmail.com>
2021-02-15 19:18:58 +0100

    Fix test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,6 @@
 import { createFluentModelBuilder } from 'src'
 
-const areDefined = (obj: any[]) => obj.forEach(obj => expect(obj).toBeDefined)
+const areDefined = (obj: any[]) => obj.forEach(obj => expect(obj).toBeDefined())
 
 it('declared things are exported', () => {
     areDefined([createFluentModelBuilder])
